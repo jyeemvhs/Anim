7bf86fc8db9606de9c22bde2b6c64eb5b0f3431d
Jeffrey Yee <jeffreyyee@vistausd.org>
2021-04-24 16:04:00 -0700

    Initial commit

---
+++ Assets/SphereScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+    public Transform target;
+    UnityEngine.AI.NavMeshAgent agent;
+    Vector3 initLoc;
+    // Use this for initialization
+    void Start()
+    {
+        initLoc = target.position;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(transform.position, target.position) > 5)
+        {
+            agent.SetDestination(transform.position);
+        }
+        else
+        {
+            agent.SetDestination(target.position);
+        }
+
+        if (Vector3.Distance(transform.position, target.position) < 1)
+        {
+            target.position = initLoc;
+        }
+    }
+}
